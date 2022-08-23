ac7378c6ecf432b70d6374315cd8297e67d3764b
RLettuce <syunrobin@gmail.com>
2022-08-23 17:07:33 +0900

    ✧FormText.vue

---
+++ components/FormText.vue
@@ -0,0 +1,10 @@
+<template>
+  <form>
+    <div>
+      <v-text-field
+        v-model="$store.state.sepalLength"
+        label="Sepal Length"
+      ></v-text-field>
+    </div>
+  </form>
+</template>
--- components/Formimg.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <form>
-    <div>
-      <input type="file" v-on:change="upload">
-    </div>
-  </form>
-</template>
