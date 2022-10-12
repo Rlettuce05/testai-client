491c743d41834f9de96d8130abd922efae060fdb
RLettuce <syunrobin@gmail.com>
2022-10-12 12:42:09 +0900

    ✒Fromimg.vue: upload func, resizeimg func

--- components/Formimg.vue
+++ components/Formimg.vue
@@ -5,7 +5,7 @@
       <v-file-input
         truncate-length="6"
         accept="image/png"
-        @change="onUpload()"
+        @change="roadimg()"
       ></v-file-input>
       <v-btn
         color="primary"
@@ -26,11 +26,29 @@ export default {
     }
   },
   methods: {
-    onUpload: function() {
+    resizeimg: function(imageData) {
+      const canvas = document.createElement('canvas');
+      canvas.width = 32;
+      canvas.height = 32;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(imageData, 0, 0, 32, 32);
+      return canvas.toDataURL('image/png')
+    },
+    roadimg: function(e) {
       this.images = event.target.files;
+      let reader = new FileReader();
+      reader.onload = () => {
+        const imgData = reader.result;
+        const resizedImgData = resizeimg(imgData);
+        this.images = resizedImgData;
+      };
     },
     callapi: function() {
-      
+      let formData = new FormData
+      formData.append('image', this.images);
+      this.$axios.$post('http://localhost:4000/api/predict',
+      formData,
+      ).then((response) => {this.$store.commit('setPredictLabel', response.data.result)})
     }
   }
 }
--- store/index.js
+++
@@ -1,2 +0,0 @@
-export const strict = false;
-
